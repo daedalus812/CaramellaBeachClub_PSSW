8b7cf96bf6abe52947d8f14792a376d9e07efb0c
Lorenzo <lorenzofanelli02@icloud.com>
2024-05-08 11:20:44 +0200

    Initial commit

---
+++ src/main/java/org/ecommerce/azemporium/AZEmporium.java
@@ -0,0 +1,13 @@
+package org.ecommerce.pssw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsswApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsswApplication.class, args);
+    }
+
+}
