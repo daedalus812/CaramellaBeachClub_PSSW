0fc1d194085608431dac6f06017beb7a14cb1144
Lorenzo <lorenzofanelli02@icloud.com>
2024-05-08 11:32:52 +0200

    Generated entities

--- src/main/java/org/ecommerce/azemporium/AZEmporium.java
+++ src/main/java/org/ecommerce/azemporium/AZEmporium.java
@@ -1,13 +1,13 @@
-package org.ecommerce.pssw;
+package org.ecommerce.azemporium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PsswApplication {
+public class AZEmporium {
 
     public static void main(String[] args) {
-        SpringApplication.run(PsswApplication.class, args);
+        SpringApplication.run(AZEmporium.class, args);
     }
 
 }
