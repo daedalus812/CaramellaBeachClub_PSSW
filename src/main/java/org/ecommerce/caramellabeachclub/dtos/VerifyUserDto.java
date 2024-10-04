d8e262bc0b413a76e6b2b92e824181cea67b8da2
daedalus128 <lorenzofanelli02@icloud.com>
2024-10-04 19:08:51 +0200

    Aggiunta verifica account tramite email

---
+++ src/main/java/org/ecommerce/caramellabeachclub/dtos/VerifyUserDto.java
@@ -0,0 +1,12 @@
+package org.ecommerce.caramellabeachclub.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VerifyUserDto {
+    private String email;
+    private String verificationCode;
+
+}
